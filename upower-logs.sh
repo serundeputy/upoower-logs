c39100418bb1071f14af50bdce962305658ea055
Geoff St. Pierre <serundeputy@gmail.com>
2021-01-22 11:32:34 -0500

    Adding a doc block.

--- upower-logs.sh
+++ upower-logs.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+###
+# Create a log file of times and battery percenage left.
+#
+# To run symlink the `upower-logs.sh` file to `/usr/local/bin/upl`
+# `ln -s PATH/TO/upower-logs.sh /usr/local/bin/upl`
+# 
+# Then you can run it by just typing `upl` at the command prompt.
+###
+
 # Get start time
 STIME=`date "+%d-%b-%Y-%m"`
 
 while true; do
   DATE=`date`
   UPOWER=`upower -d | grep percentage | head -n 1`
-  echo "${DATE} ${UPOWER}" >> "/home/gff/battery${STIME}.log"
+  echo "${DATE} ${UPOWER}" >> "${HOME}/battery${STIME}.log"
   sleep 300
   sync;
 done
