164ccbf53b137caf171f402672650b2c165af2d0
Geoff St. Pierre <serundeputy@gmail.com>
2021-01-25 08:56:07 -0500

    Adding messaging to stdout.

--- upower-logs.sh
+++ upower-logs.sh
@@ -12,9 +12,11 @@
 # Get start time
 STIME=`date "+%d-%b-%Y-%m"`
 
+echo "\tWriting logs to ${HOME}/battery${STIME}.log\n\n"
 while true; do
   DATE=`date`
   UPOWER=`upower -d | grep percentage | head -n 1`
+  echo "${DATE} ${UPOWER}"
   echo "${DATE} ${UPOWER}" >> "${HOME}/battery${STIME}.log"
   sleep 300
   sync;
